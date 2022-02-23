51569846fca8591e6843d865db9b3e5c03e1b890
Linimik <63650909+jameslinimk@users.noreply.github.com>
2022-02-23 17:32:16 -0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    token: "OTQ1ODg3MjE4MjM5NjgwNTQz.YhWsHg.xlTyPnQUbI0ZpGWOAFfEcrKOAlI",
-    tenorKey: "0MF2B6BBHH1Y"
-};
-//# sourceMappingURL=config.js.map
